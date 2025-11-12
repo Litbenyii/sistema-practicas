f9bce0a8d52051684a42ddce73ce146b82adb1b3
Litbenyii <diazb9737@gmail.com>
2025-11-12 15:11:26 -0300

    .

--- frontend/src/login.jsx
+++ frontend/src/login.jsx
@@ -41,6 +41,7 @@ export default function Login({ onLoginSuccess }) {
             </label>
             <input
               className="w-full px-4 py-2 rounded-xl border border-slate-200 focus:outline-none focus:ring-2 focus:ring-slate-900/70"
+              value={email}
               onChange={(e) => setEmail(e.target.value)}
               type="email"
             />
