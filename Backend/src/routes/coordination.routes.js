e29d25109d3640a414d98b670cb1ff0e1e4c2f53
Litbenyii <diazb9737@gmail.com>
2025-12-27 22:44:41 -0300

    .

--- Backend/src/routes/coordination.routes.js
+++ Backend/src/routes/coordination.routes.js
@@ -1,25 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-// Middleware de seguridad
 const { verifyToken, requireCoordination } = require("../middleware/auth");
 
-// Importación de todos los controladores unificados
 const {
   createOfferController,
   listOffersController,
-  deactivateOfferController,
-  createStudentController,
   listExternalRequestsController,
   approveExternalRequestController,
-  rejectExternalRequestController,
   listApplicationsController,
   approveApplicationController,
   rejectApplicationController,
-  listEvaluatorsController,
-  listOpenPracticesController,
-  assignEvaluatorController,
-  closePracticeController,
+  createStudentController,
+  deactivateOfferController,
 } = require("../controllers/coordination.controller");
 
 router.use(verifyToken);
@@ -27,22 +20,17 @@ router.use(requireCoordination);
 
 router.get("/offers", listOffersController);
 router.post("/offers", createOfferController);
+
 router.post("/offers/:id/deactivate", deactivateOfferController);
 
 router.get("/external-requests", listExternalRequestsController);
 router.post("/external-requests/:id/approve", approveExternalRequestController);
-router.post("/external-requests/:id/reject", rejectExternalRequestController);
+router.post("/practices/:id/close",authMiddleware("COORDINATION"),closePracticeController);
 
 router.get("/applications", listApplicationsController);
 router.post("/applications/:id/approve", approveApplicationController);
 router.post("/applications/:id/reject", rejectApplicationController);
 
 router.post("/students", createStudentController);
-
-router.get("/evaluators", listEvaluatorsController);
-
-router.get("/practices/open", listOpenPracticesController);
-router.post("/practices/assign-evaluator", assignEvaluatorController);
-router.post("/practices/:id/close", closePracticeController);
 
 module.exports = router;
